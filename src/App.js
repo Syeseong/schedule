f9e7548e3498b1a280581352dbb6a3af4b4daeee
Syeseong <sy051398@naver.com>
2024-04-04 16:25:50 +0900

    feat : 2024-04-04 -header 디자인

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import WeekdaySelector from './component/WeekdaySelector';
 
 function App() {
   return (
     <div className="App">
-
+      <WeekdaySelector />
     </div>
   );
 }
